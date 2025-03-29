288bd0838913f6f632bee68707dc6a8b1a50deff
Candy Montero <candymdeoleo@gmail.com>
2025-03-29 03:19:17 +0100

    feat: create toJSON mapper

--- application/media/useCases/getAllGamesUseCase.js
+++ application/media/useCases/getAllGamesUseCase.js
@@ -10,6 +10,15 @@ export class GetAllGamesUseCase {
       throw new Error("Failed to retrieve games");
     }
 
-    return allGames.map((game) => game.toJSON());
+    return allGames.map((game) => this.toJSON(game));
+  }
+
+  toJSON(game) {
+    return {
+      id: game.id,
+      title: game.title,
+      status: game.status,
+      platform: game.platform,
+    };
   }
 }
