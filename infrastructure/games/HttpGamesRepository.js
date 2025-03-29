fdebcd404fbceb29b1c0887eeff0885f2e7e9f0a
Candy Montero <candymdeoleo@gmail.com>
2025-03-29 03:21:14 +0100

    feat: create httpGameRepository

---
+++ infrastructure/games/HttpGamesRepository.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import { MediaItemRepository } from "../../domain/media/repositories/mediaItemRepository";
+
+export class HttpGamesRepository extends MediaItemRepository {
+  async getAll() {
+    // TODO: Implement
+  }
+
+  async add({ title, status, platform }) {
+    // TODO: Implement
+  }
+
+  async update({ id, title, status, platform }) {
+    // TODO: Implement
+  }
+
+  async delete({ id }) {
+    // TODO: Implement
+  }
+}
