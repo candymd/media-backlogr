fa8e17a5e0168f4dd802782663c38b0cdc955710
Candy Montero <candymdeoleo@gmail.com>
2025-03-29 18:56:12 +0100

    feat: use private properties

--- domain/media/entities/Game.js
+++ domain/media/entities/Game.js
@@ -2,25 +2,27 @@ import { MediaItem } from "./MediaItem";
 import { validatePlatform } from "../services/validatePlatformTypes";
 
 export class Game extends MediaItem {
+  #platform;
+
   constructor({ id, title, status, platform }) {
     super({ id, title, status });
     validatePlatform(platform);
-    this._platform = platform;
+    this.#platform = platform;
   }
 
   get platform() {
-    return this._platform;
+    return this.#platform;
   }
 
   set platform(value) {
     validatePlatform(value);
-    this._platform = value;
+    this.#platform = value;
   }
 
   toJSON() {
     return {
       ...super.toJSON(),
-      platform: this._platform,
+      platform: this.#platform,
     };
   }
 }
