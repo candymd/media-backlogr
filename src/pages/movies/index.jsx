6e0b4673d630efc943a5a67f671a44db9939e3ad
Candy Montero <candymdeoleo@gmail.com>
2025-06-08 17:32:09 +0200

    feat: use new form on movies page

--- src/pages/movies/index.jsx
+++ src/pages/movies/index.jsx
@@ -1,31 +1,57 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useUseCases } from "../../../application/context";
+import Modal from "../../components/modal";
+import MediaForm from "../../components/mediaForm";
+import { MEDIA_TYPES } from "../../../domain/config";
 
 export default function MoviesPage() {
   const [movies, setMovies] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const { getAllMoviesUseCase } = useUseCases();
 
-  useEffect(() => {
-    const fetchMovies = async () => {
+  const fetchMovies = useCallback(async () => {
+    try {
       const movies = await getAllMoviesUseCase.execute();
       setMovies(movies);
-    };
-
-    fetchMovies();
+    } catch (error) {
+      // TODO: handle error
+      console.error(error);
+    }
   }, [getAllMoviesUseCase]);
+
+  useEffect(() => {
+    fetchMovies();
+  }, [fetchMovies]);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
 
   return (
     <>
-      <head>
-        <title>Movies</title>
-        <meta name="description" content="Movies" />
-      </head>
-      <h1>Movies</h1>
-      <ul>
+      {isModalOpen && (
+        <Modal
+          open={isModalOpen}
+          onClose={toggleModal}
+          header="Log a new movie"
+        >
+          <MediaForm type={MEDIA_TYPES.MOVIE} onSubmit={() => fetchMovies()} />
+        </Modal>
+      )}
+      <div className="container mx-auto p-4">
+        <h1 className="text-3xl font-semibold mb-4">Movies Backlog</h1>
         {movies.map((movie) => (
-          <li key={movie.id}>{movie.title}</li>
+          <div key={movie.id}>{movie.title}</div>
         ))}
-      </ul>
+        <div className="mb-6">
+          <button
+            onClick={toggleModal}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Add New Movie
+          </button>
+        </div>
+      </div>
     </>
   );
 }
