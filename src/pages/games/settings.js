26ecbe5a372b3eb7e06f67815915f8c26084a9c3
Candy Montero <candymdeoleo@gmail.com>
2025-06-09 18:01:03 +0200

    feat: use new grid and cards on pages

---
+++ src/pages/games/settings.js
@@ -0,0 +1,10 @@
+export const mapPlatformLabel = (platform) => {
+  switch (platform) {
+    case "PC":
+      return "PC";
+    case "PLAYSTATION_5":
+      return "PS5";
+    case "XBOX":
+      return "Xbox";
+  }
+};
