1d6f20fd244ae311c0ba876d69e3d379f7ba59ee
Candy Montero <candymdeoleo@gmail.com>
2025-06-09 17:59:53 +0200

    feat: create media grid

---
+++ src/components/media/grid/index.jsx
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types";
+import MediaCard from "../card/index";
+
+function MediaGrid({ type, items }) {
+  if (!items?.length) {
+    return (
+      <div className="text-center py-12">
+        <p className="text-gray-400 text-lg">
+          No {type.toLowerCase()}s found. Add your first {type.toLowerCase()}!
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="mb-8 grid grid-cols-4 gap-2 lg:grid-cols-5">
+      {items.map((item) => (
+        <MediaCard key={item.id} type={type} item={item} />
+      ))}
+    </div>
+  );
+}
+
+MediaGrid.propTypes = {
+  type: PropTypes.string.isRequired,
+  items: PropTypes.array.isRequired,
+};
+
+export default MediaGrid;
